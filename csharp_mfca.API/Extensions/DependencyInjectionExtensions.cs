a978846c710ecd4dcddd1274dd8c2f823ff04375
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-14 06:21:24 +0630

    -

--- csharp_mfca.API/Extensions/DependencyInjectionExtensions.cs
+++ csharp_mfca.API/Extensions/DependencyInjectionExtensions.cs
@@ -10,86 +10,85 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace csharp_mfca.API.Extensions
+namespace csharp_mfca.API.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
 
-            builder.Services.AddControllers()
-                .ConfigureApiBehaviorOptions(opt =>
-                {
-                    opt.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context
-                            .ModelState.Where(ms => ms.Value!.Errors.Any())
-                            .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
-                            .ToList();
-
-                        var errorMessage = string.Join(". ", errors);
-                        var result = BaseResponse<object>.Fail(context.HttpContext.TraceIdentifier, errorMessage);
-
-                        return new OkObjectResult(result);
-                    };
-                })
-                .AddJsonOptions(opt =>
-                {
-                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                    opt.JsonSerializerOptions.DictionaryKeyPolicy = null;
-                });
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHealthChecks();
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.AddScoped<IPasswordHasher<TblUser>, PasswordHasher<TblUser>>();
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
-            builder.Services.AddBusinessLogicServices();
-            builder.Services.AddDataAccessServices();
-            builder.Services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
-            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-            builder.Services.AddProblemDetails();
-
-            builder.Services.AddDbContext<AppDbContext>(opt =>
+        builder.Services.AddControllers()
+            .ConfigureApiBehaviorOptions(opt =>
             {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                opt.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context
+                        .ModelState.Where(ms => ms.Value!.Errors.Any())
+                        .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
+                        .ToList();
+
+                    var errorMessage = string.Join(". ", errors);
+                    var result = BaseResponse<object>.Fail(context.HttpContext.TraceIdentifier, errorMessage);
+
+                    return new OkObjectResult(result);
+                };
+            })
+            .AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+                opt.JsonSerializerOptions.DictionaryKeyPolicy = null;
             });
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    "CORSPolicy",
-                    builder =>
-                        builder
-                            .AllowAnyMethod()
-                            .AllowAnyHeader()
-                            .AllowCredentials()
-                            .SetIsOriginAllowed((hosts) => true)
-                );
-            });
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<IPasswordHasher<TblUser>, PasswordHasher<TblUser>>();
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
+        builder.Services.AddBusinessLogicServices();
+        builder.Services.AddDataAccessServices();
+        builder.Services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
 
-            return services;
-        }
-
-        public static IApplicationBuilder UseTraceMiddleware(this WebApplication app)
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            return app.UseMiddleware<TraceIdMiddleware>();
-        }
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-        private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+        builder.Services.AddCors(options =>
         {
-            return services.AddScoped<BL_CreateUser>();
-        }
+            options.AddPolicy(
+                "CORSPolicy",
+                builder =>
+                    builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .SetIsOriginAllowed((hosts) => true)
+            );
+        });
 
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            return services.AddScoped<DA_CreateUser>();
-        }
+        return services;
+    }
+
+    public static IApplicationBuilder UseTraceMiddleware(this WebApplication app)
+    {
+        return app.UseMiddleware<TraceIdMiddleware>();
+    }
+
+    private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_CreateUser>();
+    }
+
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        return services.AddScoped<DA_CreateUser>();
     }
 }
