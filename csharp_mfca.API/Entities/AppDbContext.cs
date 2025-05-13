1308bac2adf3542e613d4642134b384b97514df9
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-14 06:20:54 +0630

    -

--- csharp_mfca.API/Entities/AppDbContext.cs
+++ csharp_mfca.API/Entities/AppDbContext.cs
@@ -5,9 +5,7 @@ namespace csharp_mfca.API.Entities;
 public partial class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     public virtual DbSet<TblUser> TblUsers { get; set; }
 
@@ -19,25 +17,13 @@ public partial class AppDbContext : DbContext
 
             entity.ToTable("tbl_users");
 
-            entity.Property(e => e.UserId)
-                .HasMaxLength(50)
-                .HasColumnName("user_id");
-            entity.Property(e => e.CreatedAt)
-                .HasColumnType("datetime")
-                .HasColumnName("created_at");
-            entity.Property(e => e.Email)
-                .HasMaxLength(150)
-                .HasColumnName("email");
-            entity.Property(e => e.FullName)
-                .HasMaxLength(50)
-                .HasColumnName("full_name");
+            entity.Property(e => e.UserId).HasMaxLength(50).HasColumnName("user_id");
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime").HasColumnName("created_at");
+            entity.Property(e => e.Email).HasMaxLength(150).HasColumnName("email");
+            entity.Property(e => e.FullName).HasMaxLength(50).HasColumnName("full_name");
             entity.Property(e => e.IsDeleted).HasColumnName("is_deleted");
-            entity.Property(e => e.PasswordHash)
-                .HasMaxLength(150)
-                .HasColumnName("password_hash");
-            entity.Property(e => e.Salt)
-                .HasMaxLength(50)
-                .HasColumnName("salt");
+            entity.Property(e => e.PasswordHash).HasMaxLength(150).HasColumnName("password_hash");
+            entity.Property(e => e.Salt).HasMaxLength(50).HasColumnName("salt");
         });
 
         OnModelCreatingPartial(modelBuilder);
