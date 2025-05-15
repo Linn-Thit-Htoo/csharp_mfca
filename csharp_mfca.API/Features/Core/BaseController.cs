b8687ffaf5115aa08db4f5ba55f2383519f52051
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-15 07:32:38 +0630

    -

--- csharp_mfca.API/Features/Core/BaseController.cs
+++ csharp_mfca.API/Features/Core/BaseController.cs
@@ -2,13 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace csharp_mfca.API.Features.Core
+namespace csharp_mfca.API.Features.Core;
+
+public class BaseController : ControllerBase
 {
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
