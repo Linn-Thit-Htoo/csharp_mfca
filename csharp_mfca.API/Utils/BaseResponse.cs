a7523ce9f71b896a58ce986ec68e603df29a5085
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-15 23:22:09 +0630

    -

--- csharp_mfca.API/Utils/BaseResponse.cs
+++ csharp_mfca.API/Utils/BaseResponse.cs
@@ -1,62 +1,61 @@
 ﻿using csharp_mfca.API.Constants;
 using System.Net;
 
-namespace csharp_mfca.API.Utils
+namespace csharp_mfca.API.Utils;
+
+public class BaseResponse<T>
 {
-    public class BaseResponse<T>
+    public string RefNo { get; set; }
+    public string Message { get; set; }
+    public bool IsSuccess { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+    public T? Data { get; set; }
+
+    public static BaseResponse<T> Success(string refNo, string message = MessageConstant.Success, HttpStatusCode statusCode = HttpStatusCode.OK)
     {
-        public string RefNo { get; set; }
-        public string Message { get; set; }
-        public bool IsSuccess { get; set; }
-        public HttpStatusCode StatusCode { get; set; }
-        public T? Data { get; set; }
-
-        public static BaseResponse<T> Success(string refNo, string message = MessageConstant.Success, HttpStatusCode statusCode = HttpStatusCode.OK)
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                Data = default,
-                IsSuccess = true,
-                Message = message,
-                StatusCode = statusCode,
-                RefNo = refNo
-            };
-        }
+            Data = default,
+            IsSuccess = true,
+            Message = message,
+            StatusCode = statusCode,
+            RefNo = refNo
+        };
+    }
 
-        public static BaseResponse<T> Success(string refNo, T data, string message = MessageConstant.Success, HttpStatusCode statusCode = HttpStatusCode.OK)
+    public static BaseResponse<T> Success(string refNo, T data, string message = MessageConstant.Success, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                Data = data,
-                IsSuccess = true,
-                Message = message,
-                StatusCode = statusCode,
-                RefNo = refNo
-            };
-        }
+            Data = data,
+            IsSuccess = true,
+            Message = message,
+            StatusCode = statusCode,
+            RefNo = refNo
+        };
+    }
 
-        public static BaseResponse<T> Fail(string refNo, string message = MessageConstant.Failed, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    public static BaseResponse<T> Fail(string refNo, string message = MessageConstant.Failed, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                Data = default,
-                IsSuccess = false,
-                Message = message,
-                StatusCode = statusCode,
-                RefNo = refNo
-            };
-        }
+            Data = default,
+            IsSuccess = false,
+            Message = message,
+            StatusCode = statusCode,
+            RefNo = refNo
+        };
+    }
 
-        public static BaseResponse<T> Fail(string refNo, Exception ex, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    public static BaseResponse<T> Fail(string refNo, Exception ex, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                Data = default,
-                IsSuccess = false,
-                Message = ex.ToString(),
-                StatusCode = statusCode,
-                RefNo = refNo
-            };
-        }
+            Data = default,
+            IsSuccess = false,
+            Message = ex.ToString(),
+            StatusCode = statusCode,
+            RefNo = refNo
+        };
     }
 }
