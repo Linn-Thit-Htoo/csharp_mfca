90187a836b538349fab677854c50d2573df0f3e0
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-15 07:32:26 +0630

    -

--- csharp_mfca.API/Extensions/Mapper.cs
+++ csharp_mfca.API/Extensions/Mapper.cs
@@ -1,22 +1,21 @@
 ﻿using csharp_mfca.API.Entities;
 using csharp_mfca.API.Features.Users.CreateUser;
 
-namespace csharp_mfca.API.Extensions
+namespace csharp_mfca.API.Extensions;
+
+public static class Mapper
 {
-    public static class Mapper
+    public static TblUser ToEntity(this CreateUserRequest request, string hashPassword, string salt)
     {
-        public static TblUser ToEntity(this CreateUserRequest request, string hashPassword, string salt)
+        return new TblUser
         {
-            return new TblUser
-            {
-                UserId = Ulid.NewUlid().ToString(),
-                FullName = request.FullName,
-                Email = request.Email,
-                CreatedAt = DateTime.Now,
-                IsDeleted = false,
-                PasswordHash = hashPassword,
-                Salt = salt
-            };
-        }
+            UserId = Ulid.NewUlid().ToString(),
+            FullName = request.FullName,
+            Email = request.Email,
+            CreatedAt = DateTime.Now,
+            IsDeleted = false,
+            PasswordHash = hashPassword,
+            Salt = salt
+        };
     }
 }
