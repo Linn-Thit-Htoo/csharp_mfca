828f07232727c099ca7ab1ae4e53431f21693de8
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-15 07:32:51 +0630

    -

--- csharp_mfca.API/Middlewares/TraceIdMiddleware.cs
+++ csharp_mfca.API/Middlewares/TraceIdMiddleware.cs
@@ -1,18 +1,17 @@
-﻿namespace csharp_mfca.API.Middlewares
+﻿namespace csharp_mfca.API.Middlewares;
+
+public class TraceIdMiddleware(RequestDelegate next)
 {
-    public class TraceIdMiddleware(RequestDelegate next)
+    internal readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext httpContext)
     {
-        internal readonly RequestDelegate _next = next;
+        httpContext.Request.Headers.TryGetValue("X-Trace-Id", out var headerValue);
+        httpContext.TraceIdentifier = string.IsNullOrEmpty(headerValue)
+            ? Guid.NewGuid().ToString()
+            : headerValue!;
 
-        public async Task InvokeAsync(HttpContext httpContext)
-        {
-            httpContext.Request.Headers.TryGetValue("X-Trace-Id", out var headerValue);
-            httpContext.TraceIdentifier = string.IsNullOrEmpty(headerValue)
-                ? Guid.NewGuid().ToString()
-                : headerValue!;
-
-            httpContext.Response.Headers["X-Trace-Id"] = httpContext.TraceIdentifier;
-            await _next(httpContext);
-        }
+        httpContext.Response.Headers["X-Trace-Id"] = httpContext.TraceIdentifier;
+        await _next(httpContext);
     }
 }
